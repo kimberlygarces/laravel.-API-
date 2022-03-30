b9b62f43cd3535b684efb48dea732ab18726986a
Kimberly Garcés Pérez <kimberly.gp@hotmail.com>
2022-03-29 19:58:20 -0500

    modelo de agenda

--- app/agenda.php
+++ app/agenda.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class agenda extends Model
 {
-        //clase que extiende del modelo - pasamos los campos van hacer llenados por el modelo hacia la BD
         protected $fillable = ["asunto","fecha","hora","estado", "cliente_id"];
+        //clase que extiende del modelo - pasamos los campos van hacer llenados por el modelo hacia la BD
+
 }
